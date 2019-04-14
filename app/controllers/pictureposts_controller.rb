423fc4b5988eeddd394abc7637e58a75cf1db6c3
Takuho Tada <pieeeeeta@gmail.com>
2019-04-14 10:34:20 +0900

    add delete picturepost
    写真の削除機能追加

--- app/controllers/pictureposts_controller.rb
+++ app/controllers/pictureposts_controller.rb
@@ -1,6 +1,7 @@
 class PicturepostsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :delete]
+  before_action :correct_user, only: :destroy
 
   def show
     @picturepost = Picturepost.find(params[:id])
@@ -18,9 +19,19 @@ class PicturepostsController < ApplicationController
     redirect_to root_url
   end
 
+  def destroy
+    @picturepost.destroy
+    redirect_to user_path(@picturepost.user)
+  end
+
   private
 
   def picturepost_params
     params.require(:picturepost).permit(:content, :picture)
+  end
+
+  def correct_user
+    @picturepost = current_user.pictureposts.find_by(id: params[:id])
+    redirect_to root_url if @picturepost.nil?
   end
 end
--- app/models/picturepost.rb
+++ app/models/picturepost.rb
@@ -1,5 +1,5 @@
 class Picturepost < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   mount_uploader :picture, PictureUploader
 end
